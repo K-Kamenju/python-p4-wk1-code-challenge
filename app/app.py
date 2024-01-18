da99ac473ff42478d4473e8d5913ff9f7c9ef98e
Caleb <kamenjuhkiiru@gmail.com>
2024-01-18 23:48:03 +0300

    Power Routes

--- app/app.py
+++ app/app.py
@@ -33,10 +33,47 @@ def heroes():
 
 @app.route('/heroes/<int:id>')
 def heroes_by_id(id):
+     
+    hero = Hero.query.filter_by(id=id).first()
+
+    if hero:
+        hero_data = hero.to_dict()
+        return make_response(jsonify(hero_data), 200)
     
-    hero_data = Hero.query.filter_by(id=id).first().to_dict()
-    return make_response(jsonify(hero_data), 200)
+    else:
+        error_response = {"error": "Hero not found"}
+        return make_response(jsonify(error_response), 404)
  
+@app.route('/powers')
+def powers():
+    
+    powers_list =[]
+    for power in Power.query.all():
+        power_data = {
+            "id": power.id,
+            "name": power.name,
+            "description": power.description
+        }
+        powers_list.append(power_data)
+    
+    return make_response(jsonify(powers_list), 200)
+
+@app.route('/powers/<int:id>')
+def powers_by_id(id):
+     
+    power = Power.query.filter_by(id=id).first()
+
+    if power:
+        power_data = {
+            "id": power.id,
+            "name": power.name,
+            "description": power.description
+        }
+        return make_response(jsonify(power_data), 200)
+    
+    else:
+        error_response = {"error": "Power not found"}
+        return make_response(jsonify(error_response), 404)
 
 if __name__ == '__main__':
     app.run(port=5555)
